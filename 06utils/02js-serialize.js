dc79c379fa778580d3780153d08e4eb7f00185f2
SedationH <sedationh@gmail.com>
2020-09-14 22:03:55 +0800

    🔥 css

--- 06utils/02js-serialize.js
+++
@@ -1,7 +0,0 @@
-export function serialize(data) {
-  const query = Object.keys(data)
-    .map(key => data[key] && `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&')
-
-  return `?${query}`
-}
